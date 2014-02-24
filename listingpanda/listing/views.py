37947aa97039b554e42b2cec817e56661e121780
Sonny Kwok <sonny.kwok@capitalone.com>
2014-02-23 19:40:07 -0500

    added a basic page of masonry

--- listingpanda/listing/views.py
+++ listingpanda/listing/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import View
+from listingpanda.settings import BASE_DIR
+
+import json
+import os
 
 # Create your views here.
+class ListingView(View):
+
+    template_name = 'home.html'
+    sample_json = os.path.join(BASE_DIR, 'templates', 'sample.json')
+
+    def load(self):
+        json_data = open(self.sample_json)
+     	listings = json.load(json_data)
+        json_data.close()
+        return listings
+
+    def get(self, request, *args, **kwargs):
+        context = self.load()
+        return render(request, self.template_name, context)
--- listingpanda/urls.py
+++ listingpanda/urls.py
@@ -2,13 +2,14 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
+from listingpanda.listing.views import ListingView
 from listingpanda.settings import MEDIA_URL, MEDIA_ROOT
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', ListingView.as_view(), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
