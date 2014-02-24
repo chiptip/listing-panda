a9fc616d6d1290c4b24c7b56f288cd93e34a6c7d
Sonny Kwok <sonny.kwok@capitalone.com>
2014-02-23 21:59:48 -0500

    corrected content settigns for qa

--- listingpanda/settings_qa.py
+++ listingpanda/settings_qa.py
@@ -10,4 +10,4 @@ MEDIA_ROOT = '/home/scorio/webapps/beta_content'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/content/'
+MEDIA_URL = 'http://beta-content.listingpanda.com/'
